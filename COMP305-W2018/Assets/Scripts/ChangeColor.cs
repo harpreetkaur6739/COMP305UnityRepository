4d9c304ffb1b22358479903d24d15c7acf9b060d
Harpreet <harpreet.kaur6739@gmail.com>
2018-02-04 22:41:15 -0500

    Lab 2 Scene

---
+++ COMP305-W2018/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+    public GameObject character;
+    private Rigidbody2D rBody;
+    public Sprite pinkSprite;
+    public Sprite greenSprite;
+    public Sprite blueSprite;
+    private SpriteRenderer sRenderer;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Entered");
+        rBody = character.GetComponent<Rigidbody2D>();
+        sRenderer = rBody.GetComponent<SpriteRenderer>();
+
+    }
+	
+	
+	void OnMouseDown () {
+        Debug.Log("Clicked");
+        Debug.Log(this.name);
+        if (this.name == "Btn_Blue") {
+            sRenderer.sprite = blueSprite;
+        } else if (this.name == "Btn_Green") {
+            sRenderer.sprite = greenSprite;
+        } else if (this.name == "Btn_Pink") {
+            sRenderer.sprite = pinkSprite;
+        }
+    }
+}
